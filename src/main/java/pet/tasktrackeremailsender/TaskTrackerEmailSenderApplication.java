2bf7b72ff3e2ce501819a8e22713882b28654983
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-07-26 18:35:14 +0200

    initial commit

---
+++ src/main/java/pet/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package pet.tasktrackeremailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/tasktrackeremailsender/TaskTrackerEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.tasktrackeremailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
