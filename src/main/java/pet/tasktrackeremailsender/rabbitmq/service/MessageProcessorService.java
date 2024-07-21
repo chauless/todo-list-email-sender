5df6174fc2298dd3030286972b71e5f8b98923b4
KovalovDm <kovaldmy@fel.cvut.cz>
2024-07-22 00:40:13 +0200

    add javadoc and interfaces above services

--- src/main/java/pet/tasktrackeremailsender/rabbitmq/service/MessageProcessorService.java
+++ src/main/java/pet/tasktrackeremailsender/rabbitmq/service/MessageProcessorService.java
@@ -1,31 +1,18 @@
 package pet.tasktrackeremailsender.rabbitmq.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.amqp.core.Message;
-import pet.tasktrackeremailsender.dto.EmailDto;
-import pet.tasktrackeremailsender.mail.service.EmailSenderService;
-import pet.tasktrackeremailsender.rabbitmq.service.chainofresponsibility.EmailMessageHandler;
-import pet.tasktrackeremailsender.rabbitmq.service.chainofresponsibility.MessageHandler;
 
 import java.io.IOException;
 
-@Service
-@RequiredArgsConstructor
-public class MessageProcessorService {
-
-    private final EmailSenderService emailSenderService;
-    private final ObjectMapper objectMapper;
-
-    private MessageHandler buildChain() {
-        MessageHandler emailHandler = new EmailMessageHandler(emailSenderService, objectMapper);
-
-        return emailHandler;
-    }
-
-    public void processMessage(Message message) throws IOException {
-        MessageHandler handlerChain = buildChain();
-        handlerChain.handle(message);
-    }
+public interface MessageProcessorService {
+    /**
+     * Processes the given message.
+     *
+     * This method builds a chain of responsibility for handling the message and then passes the message to it.
+     * The chain of responsibility is built using the private buildChain() method.
+     *
+     * @param message the message to be processed
+     * @throws IOException if an I/O error occurs during message processing
+     */
+    void processMessage(Message message) throws IOException;
 }
---
+++ src/main/java/pet/tasktrackeremailsender/rabbitmq/service/MessageProcessorServiceImpl.java
@@ -0,0 +1,30 @@
+package pet.tasktrackeremailsender.rabbitmq.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.amqp.core.Message;
+import pet.tasktrackeremailsender.mail.service.EmailSenderService;
+import pet.tasktrackeremailsender.rabbitmq.service.chainofresponsibility.EmailMessageHandler;
+import pet.tasktrackeremailsender.rabbitmq.service.chainofresponsibility.MessageHandler;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class MessageProcessorServiceImpl implements MessageProcessorService {
+
+    private final EmailSenderService emailSenderService;
+    private final ObjectMapper objectMapper;
+
+    private MessageHandler buildChain() {
+        MessageHandler emailHandler = new EmailMessageHandler(emailSenderService, objectMapper);
+
+        return emailHandler;
+    }
+
+    public void processMessage(Message message) throws IOException {
+        MessageHandler handlerChain = buildChain();
+        handlerChain.handle(message);
+    }
+}
